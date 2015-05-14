734e9a741e38aefd6fe9cfccde9f556234c03464
Jamie Rees <jamierees@outlook.com>
2015-05-14 19:36:35 +0100

    Started adding UnitTests

---
+++ Yak.Tests/Converters/BoolToInverseVisibilityConverterTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows;
+using NUnit.Framework;
+using Yak.Converters;
+
+namespace Yak.Tests.Converters
+{
+    [TestFixture]
+    public class BoolToInverseVisibilityConverterTest
+    {
+        [Test]
+        public void BoolToInverseConvertTest()
+        {
+            var converter = new BoolToInverseVisibilityConverter();
+            Assert.That(converter.Convert(true, typeof(Visibility), null, CultureInfo.CurrentUICulture).Equals(Visibility.Visible));
+            Assert.That(converter.Convert(false, typeof(Visibility), null, CultureInfo.CurrentUICulture).Equals(Visibility.Collapsed));
+        }
+    }
+}
---
+++ Yak.Tests/Converters/GenresConverterTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Yak.Converters;
+
+namespace Yak.Tests.Converters
+{
+    [TestFixture]
+    public class GenresConverterTest
+    {
+        [Test]
+        public void GenresConvertTest()
+        {
+            var converter = new GenresConverter();
+            var value = new Fixture().Create<List<string>>();
+            var converted = converter.Convert(value, null, null, CultureInfo.CurrentUICulture);
+
+            Assert.That(converted, Is.Not.Null);
+
+            foreach (var item in value)
+            {
+                Assert.That(converted,Is.StringContaining(item));
+            }
+        }
+    }
+}
